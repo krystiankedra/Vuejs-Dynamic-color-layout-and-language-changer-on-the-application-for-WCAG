dca65a77b6aebf039f50df3ca4b69793e2a11f38
Kedra <krystian.kedra@atos.net>
2020-01-29 13:30:49 +0100

    Fixed typo

--- pages/employees/index.vue
+++ pages/employees/index.vue
@@ -10,7 +10,7 @@
 
 
 <script>
-import { asyncEmployeesWrapper } from '~/common/Components/AsyncComponents/AsyncComponents'
+import { asyncEmployeesWrapper } from '~/common/Components/AsyncComponents/asyncComponents'
 export default {
   components: {
     asyncEmployeesWrapper
